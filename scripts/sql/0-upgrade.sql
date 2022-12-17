7d51fcf5f75825860878611d34a9b66370ca0dec
matehaxor03 <matehaxor03>
2022-12-17 19:56:03 +1100

    continue adjusting data struture

--- scripts/sql/0-upgrade.sql
+++ scripts/sql/0-upgrade.sql
@@ -10,6 +10,20 @@ CREATE TABLE IF NOT EXISTS `DomainName` (
 
 INSERT INTO `DomainName` (`name`) VALUES ('github.com');
 
+CREATE TABLE IF NOT EXISTS `BuildStep` (
+    `build_step_id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    `name` VARCHAR(255) NOT NULL DEFAULT '' comment '{"rules":["build_step_name"]}', 
+    `order` BIGINT NOT NULL, 
+    `active` BOOLEAN DEFAULT 1, 
+    `archieved` BOOLEAN DEFAULT 0, 
+    `created_date` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+    `last_modified_date` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+    `archieved_date` TIMESTAMP(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
+     CONSTRAINT UC_BuildStep_name UNIQUE (`name`));
+
+INSERT INTO `BuildStep` (`name`,`order`) VALUES ('lint', 0);
+INSERT INTO `BuildStep` (`name`,`order`) VALUES ('run unit tests', 100);
+
 CREATE TABLE IF NOT EXISTS `RepositoryAccount` (
     `repository_account_id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     `name` VARCHAR(255) NOT NULL DEFAULT '' comment '{"rules":["repository_account_name"]}', 
@@ -40,22 +54,33 @@ CREATE TABLE IF NOT EXISTS `Branch` (
     `archieved_date` TIMESTAMP(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
     CONSTRAINT UC_Branch_name UNIQUE (`name`));
 
-CREATE TABLE IF NOT EXISTS `BuildBranch` (
-    `build_branch_id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS `Build` (
+    `build_id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     `domain_name_id` BIGINT UNSIGNED NOT NULL comment '{"foreign_key":{"table_name":"DomainName","column_name":"domain_name_id","type":"uint64"}}', 
     `repository_account_id` BIGINT UNSIGNED NOT NULL comment '{"foreign_key":{"table_name":"RepositoryAccount","column_name":"repository_account_id","type":"uint64"}}',
     `repository_id` BIGINT UNSIGNED NOT NULL comment '{"foreign_key":{"table_name":"Repository","column_name":"repository_id","type":"uint64"}}',
+    `active` BOOLEAN DEFAULT 1, 
+    `archieved` BOOLEAN DEFAULT 0, 
+    `created_date` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+    `last_modified_date` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+    `archieved_date` TIMESTAMP(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000', 
+    FOREIGN KEY(`domain_name_id`) REFERENCES `DomainName`(`domain_name_id`),   
+    FOREIGN KEY(`repository_account_id`) REFERENCES `RepositoryAccount`(`repository_account_id`),
+    FOREIGN KEY(`repository_id`) REFERENCES `Repository`(`repository_id`),
+    CONSTRAINT UC_BuildBranch_id UNIQUE (`domain_name_id`,`repository_account_id`,`repository_id`,`branch_id`));
+
+CREATE TABLE IF NOT EXISTS `BuildBranch` (
+    `build_branch_id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    `build_id` BIGINT UNSIGNED NOT NULL comment '{"foreign_key":{"table_name":"Build","column_name":"build_id","type":"uint64"}}', 
     `branch_id` BIGINT UNSIGNED NOT NULL comment '{"foreign_key":{"table_name":"Branch","column_name":"branch_id","type":"uint64"}}', 
     `active` BOOLEAN DEFAULT 1, 
     `archieved` BOOLEAN DEFAULT 0, 
     `created_date` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
     `last_modified_date` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
     `archieved_date` TIMESTAMP(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000', 
+    FOREIGN KEY(`build_id`) REFERENCES `Build`(`build_id`),
     FOREIGN KEY(`branch_id`) REFERENCES `Branch`(`branch_id`),
-    FOREIGN KEY(`domain_name_id`) REFERENCES `DomainName`(`domain_name_id`),
-    FOREIGN KEY(`repository_account_id`) REFERENCES `RepositoryAccount`(`repository_account_id`),
-    FOREIGN KEY(`repository_id`) REFERENCES `Repository`(`repository_id`),
-    CONSTRAINT UC_BuildBranch_id UNIQUE (`domain_name_id`,`repository_account_id`,`repository_id`,`branch_id`));
+    CONSTRAINT UC_BuildBranch_id UNIQUE (`build_id`,`branch_id`));
 
 CREATE TABLE IF NOT EXISTS `BuildBranchInstance` (
     `build_branch_instance_id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
